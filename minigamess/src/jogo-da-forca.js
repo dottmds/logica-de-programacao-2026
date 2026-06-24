b20cb5e506d47c1ce9234d72aa6bf0f0e84642a4
DOUGLAS MORAES DA SILVA <douglas_silva172@estudante.sesisenai.org.br>
2026-06-23 21:44:45 -0300

    mais

--- minigamess/src/jogo-da-forca.js
+++ minigamess/src/jogo-da-forca.js
@@ -67,5 +67,63 @@ var letrasCertas = [];
 var letrasErradas = [];
 var tentativas = 6;
 var venceu = false;
+var palavraSecreta = palavras[Math.floor(Math.random() * palavras.length)] 
+console.log(palavraSecreta)
 
-var palavraSecreta = palavras[Mathfloor(Math.random() + palavras.length)] 
+function desenhaForca(errors) {
+  console.log(partes[errors])
+}
+
+while(tentativas > 0 && venceu === false) {
+  console.clear()
+  console.log("=== JOGO DA FORCA ===")
+
+  desenhaForca(letrasErradas.length)
+
+  var palavraMontada = ''
+  for(var i = 0; i < palavraSecreta.length; i++) {
+    if(letrasCertas.includes(palavraSecreta[i])) {
+      palavraMontada += palavraSecreta[i] + " ";
+    } else {
+      palavraMontada += "_ "
+    }
+
+  }
+
+console.log("Palavra: " + palavraMontada);
+console.log("Tentativas restantes: " + tentativas)
+console.log("Letras erradas: " + letrasErradas.join(', '))
+console.log();
+
+var letra = leia.question("Digite uma letra: ").toLowerCase();
+if(letra.length !== 1) {
+  console.log("Digite apenas uma letra.")
+  leia.question("Pressionar ENTER para continuar...")
+} else if(letrasCertas.includes(letra) || letrasErradas.includes(letra)) {
+  console.log("voce ja tentou essa letra")
+  leia.question("Pressionar ENTER para continuar...")
+} else if(palavraSecreta.includes(letra)) {
+  letrasCertas.push(letra);
+} else {
+  letrasErradas.push(letra);
+  tentativas--;
+}
+
+venceu = true;
+for(var i = 0; i < palavraSecreta.length; i++) {
+  if(letrasCertas.includes(palavraSecreta[i]) === false) {
+    venceu = false;
+  }
+}
+
+}
+
+console.clear();
+console.log("=== jogo da forca ===");
+desenhaForca(letrasErradas.length);
+if(venceu === true) {
+  console.log("PARABENS VOCE VENCEU")
+} else {
+  console.log("VC PERDEU")
+  console.log("A PALAVRA CORRETA ERA: " + palavraSecreta)
+}
