fee482dde438dde927ecf533c39796ac7119d77c
DOUGLAS MORAES DA SILVA <douglas_silva172@estudante.sesisenai.org.br>
2026-08-27 20:09:19 -0300

    crud

--- CRUD-estoque/src/listar.js
+++ CRUD-estoque/src/listar.js
@@ -2,7 +2,7 @@ import { estoque } from "./estoq.js";
 
 export function listarProdutos() {
     console.log("\n------------------- ESTOQUE ATUAL -------------------");
-    console.log("ID" + " | " + "NOME" + " | " + "QTD" + " | " + "PREÇO (R$)");
+    console.log("ID".padEnd(8) + "| " + "NOME".padEnd(8) + "| " + "QTD".padEnd(8) + "| " + "PREÇO (R$)".padEnd(8));
     console.log("-----------------------------------------------------");
 
     if (estoque.length === 0) {
@@ -12,7 +12,7 @@ export function listarProdutos() {
 
     for (var i = 0; i < estoque.length; i++) {
         var produto = estoque[i];
-        console.log(produto.id + " | " + produto.nome + " | " + produto.quantidade + " | R$ " + produto.preco);
+        console.log(String(produto.id).padEnd(8) + "| " + produto.nome.padEnd(8) + "| " + String(produto.quantidade).padEnd(8) + "| R$ " + String(produto.preco).padEnd(8));
     }
     
     console.log("-----------------------------------------------------");
